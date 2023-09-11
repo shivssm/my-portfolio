cb290df9a9ccb69cf38fadc117e9936d30053fd7
Shiv shankar Mistry <shivshankar.mistry@bt.com>
2023-09-11 20:03:40 +0530

    Changing Setup

--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./MainContent.scss";
-import Intro from "../intro/Intro";
-import { Container } from "reactstrap";
 
 const MainContent: React.FC = () => {
   return (
-    <Container fluid>
-      <Intro />
-    </Container>
+    <>
+
+    </>
   )
 }
 
